3119dcadde1abb951705f7c1e97c26b1d5f5e521
daniel@biener.org <daniel@biener.org>
2017-08-04 23:45:38 -0500

    u

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.controller('MainCtrl', [
 '$scope',
 function($scope){
   
+  
   var metrics = [
     {metric: 'weight', label: 'Weight', goal: 150, unit: 'lbs', position: 0},
     {metric: 'bp', label: 'BP', goal: '120/80', unit: 'mmHg', position: 1},
